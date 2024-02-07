f2acf7dc3157f911f1f8a4bf48b633c5dc2146e2
Kauan Coli <kauanneves1008@gmail.com>
2024-02-07 15:57:59 -0300

    feat(frontend): initial Socket.IO implementation for real-time communication

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { Layout } from "@/components";
 import { Chat } from "@/pages/index";
-import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import React from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
--- frontend/src/pages/Chat/Chat.tsx
+++ frontend/src/pages/Chat/Chat.tsx
@@ -1,42 +1,66 @@
 import { useEffect, useState } from "react";
+import io from "socket.io-client";
+import { v4 as uuid } from "uuid";
+
+type MessageData = {
+  id: string;
+  message: string;
+};
+
+const socket = io("http://localhost:3000");
+socket.on("connect", () => {
+  console.log("Connected to server");
+});
 
 export const Chat = () => {
-  const [messages, setMessages] = useState<string[]>([]);
+  const [messages, setMessages] = useState<MessageData[]>([]);
   const [currentMessage, setCurrentMessage] = useState<string>("");
+  const [userId] = useState<string>(uuid());
 
-  useEffect(() => {
-    setMessages((prevMessages) => [...prevMessages]);
-  }, []);
+  const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const messageData: MessageData = { id: userId, message: currentMessage };
 
-  const sendMessage = () => {
-    setMessages((prevMessages) => [...prevMessages, currentMessage]);
+    socket.emit("message", messageData);
 
+    setMessages((prevMessages) => [...prevMessages, messageData]);
     setCurrentMessage("");
   };
+
+  useEffect(() => {
+    socket.on("message", (messageData: MessageData) => {
+      setMessages((prevMessages) => [...prevMessages, messageData]);
+    });
+    return () => {
+      socket.close();
+    };
+  }, [messages]);
 
   return (
     <div className="flex flex-col h-screen justify-between p-4">
       <div className="flex-1 overflow-y-auto">
-        {messages.map((message, index) => (
-          <div key={index} className="mb-2 text-white">
-            {message}
+        {messages.map((messageData, index) => (
+          <div
+            key={index}
+            className={`mb-2 text-white ${
+              messageData.id === userId ? "text-right" : "text-left"
+            }`}
+          >
+            {`${
+              messageData.id === userId ? "You" : "User " + messageData.id
+            }: ${messageData.message}`}
           </div>
         ))}
       </div>
-      <div className="flex items-center mt-4">
+      <form className="flex items-center mt-4" onSubmit={handleFormSubmit}>
         <input
           type="text"
           value={currentMessage}
           onChange={(e) => setCurrentMessage(e.target.value)}
           className="border p-2 flex-1 mr-2"
         />
-        <button
-          className="bg-blue-500 text-white p-2 rounded"
-          onClick={sendMessage}
-        >
-          Enviar
-        </button>
-      </div>
+        <button className="bg-blue-500 text-white p-2 rounded">Enviar</button>
+      </form>
     </div>
   );
 };
