eae230140fbea35f89de99f2bc6ccf3211c55af2
Kauan Coli <kauanneves1008@gmail.com>
2024-02-07 13:45:41 -0300

    chore(config): set up TailwindCSS, TSConfig, and ViteConfig

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
+import { Home } from "@/pages/index";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { App } from "./App.tsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>
 );
--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-export const App = () => {
+export const Home = () => {
   return (
     <>
       <h1>Init</h1>
