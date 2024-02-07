d482ddbd2611a44b97482ae49f283e85e35f7b26
Kauan Coli <kauanneves1008@gmail.com>
2024-02-07 14:41:56 -0300

    chore(config): add colors to Tailwind setup, install socket.io-client and uuid libraries

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#615EF0",
+        background: "#131619",
+      },
+    },
   },
   plugins: [],
 };
