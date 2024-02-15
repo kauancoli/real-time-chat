cd9f728d209f719b492426f10ee8c0e22961e401
Kauan Coli <kauanneves1008@gmail.com>
2024-02-15 15:22:06 -0300

    fix(frontend): baseURL from JSON-SERVER

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "https://json-server-tau-six.vercel.app/";
+const baseURL = "https://json-server-yjdx.onrender.com";
 
 export const api = axios.create({
   baseURL: baseURL,
