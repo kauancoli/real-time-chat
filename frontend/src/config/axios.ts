ee361bdd419742610470c6b0b25b2a4cf5f9f60b
Kauan Coli <kauanneves1008@gmail.com>
2024-02-15 14:48:15 -0300

    feat(frontend): add logo in favicon

--- frontend/src/config/axios.ts
+++ frontend/src/config/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseURL = "http://localhost:3001";
+const baseURL = "https://json-server-tau-six.vercel.app/";
 
 export const api = axios.create({
   baseURL: baseURL,
--- frontend/src/contexts/Sockets/SocketConnect.ts
+++ frontend/src/contexts/Sockets/SocketConnect.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-export const SocketConnect = io("http://localhost:3000");
+export const SocketConnect = io("https://chat-real-time-pcdg.onrender.com");
 SocketConnect.on("connect", () => {
   console.log("Connected to server");
 });
